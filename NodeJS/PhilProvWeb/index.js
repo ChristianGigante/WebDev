1aecf32d97d8f1e44184edead4caf9afc45465b5
Christian Gigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-19 16:35:13 +0800

    requestCounterUpdate

--- NodeJS/PhilProvWeb/index.js
+++ NodeJS/PhilProvWeb/index.js
@@ -8,16 +8,16 @@ app.set('view engine', 'pug');
 app.set('views', 'view');
 
 
-app.get('/', function (req, res) {
-    res.render('index');
-})
+// app.get('/', function (req, res) {
+//     res.render('index');
+// })
 
 
 app.get('/province/:id', function (req, res) {
     var id = req.params.id;
     fs.readFile('./provinces/' + id + '.json', function (err, data) {
         if (err) {
-            throw(err);
+            console.log(err);
         } else {
             var myData = JSON.parse(data);
             // console.log(myData);
@@ -26,9 +26,18 @@ app.get('/province/:id', function (req, res) {
     })
 })
 
-// app.get('*', function (req, res) {
-//     res.sendFile(404);
-// })
+app.get('/*/*/*/*/*', function (req, res) {
+    fs.readFile('request.txt', function (err, data) {
+        if (err) console.log(err);
+        data = data * 1;
+        data++;
+        fs.writeFile('request.txt', data, function (err) {
+            if (err) console.log(err);
+            console.log("request Added!")
+        })
+        res.end();
+    })
+})
 
 app.listen(port, function () {
     console.log("Listening to " + port);
