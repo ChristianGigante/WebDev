47bb6e5354477209c5c8b66d4add1a4f73e9e923
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-19 09:53:06 +0800

    initial commit

---
+++ NodeJS/PhilProvWeb/index.js
@@ -0,0 +1,33 @@
+const port = 9000;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.set('view engine', 'pug');
+app.set('views', 'view');
+
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+
+app.get('/province/:id', function (req, res) {
+    var id = req.params.id;
+    fs.readFile('./provinces/' + id + '.json', function (err, data) {
+        if (err) {
+            res.sendStatus(404);
+        } else {
+            var myData = JSON.parse(data);
+            res.render('index', myData);
+        }
+    })
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(404);
+})
+
+app.listen(port, function () {
+    console.log("Listening to " + port);
+});
