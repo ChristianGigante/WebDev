1a7f4002827cd993e2c468f87740c36e9e0f164d
Christian Gigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-19 15:47:06 +0800

    Pug Added
    Kayasa ahh kalisod sa kinabuhe..,

--- NodeJS/PhilProvWeb/index.js
+++ NodeJS/PhilProvWeb/index.js
@@ -1,8 +1,9 @@
-const port = 9000;
+const port = 8080;
 const express = require('express');
 const app = express();
 const fs = require('fs');
-
+const path = require('path');
+app.use(express.static(path.resolve('./public')))
 app.set('view engine', 'pug');
 app.set('views', 'view');
 
@@ -16,17 +17,18 @@ app.get('/province/:id', function (req, res) {
     var id = req.params.id;
     fs.readFile('./provinces/' + id + '.json', function (err, data) {
         if (err) {
-            res.sendStatus(404);
+            throw(err);
         } else {
             var myData = JSON.parse(data);
+            // console.log(myData);
             res.render('index', myData);
         }
     })
 })
 
-app.get('*', function (req, res) {
-    res.sendFile(404);
-})
+// app.get('*', function (req, res) {
+//     res.sendFile(404);
+// })
 
 app.listen(port, function () {
     console.log("Listening to " + port);
